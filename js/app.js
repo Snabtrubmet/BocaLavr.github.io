018021e99ef4525ace675b9b66c863808cc2d236
BocaLavr <108471752+BocaLavr@users.noreply.github.com>
2022-07-02 15:57:37 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -123,7 +123,7 @@ async function getAccount (){
 const accounts = await ethereum.request({method: 'eth_requestAccounts'});
     account = accounts[0];
   
-    showAccount.innerHTML = account.substring(0, 6)+ "..." + account.substring(account.length - 5); 
+    showAccount.innerHTML = account.substring(0, 4)+ "...." + account.substring(account.length - 4); 
     showAccount.style.color = 'rgb(143 167 255)'
 
 //***************** New Function name  *****************/
