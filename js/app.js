061b2bad14bd2d95d67f2eea7594f02994779c96
Snabtrubmet <105576834+Snabtrubmet@users.noreply.github.com>
2023-02-24 14:22:26 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -156,7 +156,7 @@ document.getElementById("add").innerHTML = contr.substring(0, 5)+ "...." + contr
 //***************** some beauty jewelry  *****************/
 
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 5, 2022 07:37:25").getTime();
+var countDownDate = new Date("Aug 5, 2023 07:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
